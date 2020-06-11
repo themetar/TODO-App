74c498edbcc4e5854e3878569f4e818aa3d92b97
Dimitar Apostolovski <d.apostolovski@gmail.com>
2020-06-11 22:16:12 +0200

    Add (Web) Storage utility

---
+++ src/modules/storage.js
@@ -0,0 +1,32 @@
+/*
+  Storage utility
+*/
+
+const available = !!(window && window.localStorage);
+
+const readAll = function readAllObjectsFromStorage (processors = {}) {
+  if (available) {
+    const len = localStorage.length;
+    
+    for (let i = 0; i < len; i++) {
+      const key = localStorage.key(i);
+      for (let prefix in processors) {
+        if (processors.hasOwnProperty(prefix)) {
+          if (processors[prefix]) {
+            const fn = processors[prefix];
+            if (key.match("^" + prefix))
+              fn(JSON.parse(localStorage.getItem(key))); 
+          }
+        }
+      }
+    }
+  }
+};
+
+const storeItem = function storeObject (prefix, item) {
+  if (available) {
+    localStorage.setItem(prefix + item.id, JSON.stringify(item));
+  }
+}
+
+export { available, readAll, storeItem };
