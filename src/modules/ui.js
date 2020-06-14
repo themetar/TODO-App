0d23d5cb6331fd069dbd7cf29010c988efbd8bba
Dimitar Apostolovski <d.apostolovski@gmail.com>
2020-06-14 22:59:04 +0200

    Add create*Element functions to ui module

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -2,23 +2,50 @@ let projects_collection;
 
 let projects_container;
 
+const createProjectElement = function createProjectDisplayElement (project) {
+  const section = document.createElement('section');
+  section.classList.add('project');
+  section.setAttribute('id', 'project-' + project.id);
+
+  const h2 = section.appendChild(document.createElement('h2'));
+  h2.appendChild(document.createTextNode(project.title));
+
+  /*
+   project buttons here
+  */
+
+  const todos_container = section.appendChild(document.createElement('div'));
+  todos_container.classList.add('project-todos');
+
+  return section;
+};
+
+const createTodoElement = function createTodoDisplayElement (todo) {
+  const div = document.createElement('div');
+  div.classList.add('todo');
+  div.setAttribute('id', 'todo-' + todo.id);
+
+  const title = div.appendChild(document.createElement('span'));
+  title.appendChild(document.createTextNode(todo.title));
+
+  /*
+   todo buttons here
+  */
+
+  return div;
+};
+
 const initialize = function initializeUserInterface (projects) {
   projects_collection = projects;
 
   projects_container = document.querySelector("#projects-container");
 
-  projects_container.appendChild(document.createTextNode("Hello App"));
-
   projects_collection.each(project => {
-    const h1 = projects_container.appendChild(document.createElement('h1'));
-    h1.appendChild(document.createTextNode(project.title));
-    const table = projects_container.appendChild(document.createElement('table'));
+    const project_element = projects_container.appendChild(createProjectElement(project));
+    const todos_container = project_element.querySelector('.project-todos');
     project.todos.each(todo => {
-      const tr = table.appendChild(document.createElement('tr'));
-      for (let prop in todo) {
-        const td = tr.appendChild(document.createElement('td'));
-        td.appendChild(document.createTextNode(todo[prop]));
-      }
+      const todo_element = createTodoElement(todo);
+      todos_container.appendChild(todo_element);
     });
   });
 };
