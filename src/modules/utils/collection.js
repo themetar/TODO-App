3abda8725288fa1a04f94d284a6bfca8f91d452f
Dimitar Apostolovski <d.apostolovski@gmail.com>
2020-06-17 21:41:29 +0200

    Add 'find' method to collections

--- src/modules/utils/collection.js
+++ src/modules/utils/collection.js
@@ -4,6 +4,13 @@ const collection = function makeACollectionOf (of) {
   return {
     map: function (fn) { return of.map(fn); },
     each: function (fn) { of.forEach(fn); },
+    find: function (params) {
+      for (let item of of) {
+        let acc = true;
+        for (const prop in params) acc = acc && (params[prop] == item[prop]);
+        if (acc) return item;
+      }
+    },
     get length () { return of.length; },
   };
 }
