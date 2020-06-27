ea4c4a64bba1bd2832447530cd3636a142de88a3
Dimitar Apostolovski <d.apostolovski@gmail.com>
2020-06-27 23:20:48 +0200

    Use webpack-dev-server for dev deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+    host: '0.0.0.0',
   },
 };
